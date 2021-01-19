12968738cb8ed47c62d745e922db35a7acfe3701
edoardocollo <edoardo.collo@gmail.com>
2021-01-19 13:40:10 +0100

    header structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,11 @@ let app = new Vue({
     // DATABASES
     /////////////////////////////////////////
     // 0# HEADER OBJECT-database
+    header:{
+      logoImg: 'assets/img/logos/logo-light.png',
+      links:['home','courses','insctructors','event','pages','elements'],
+      icons:['fas fa-search','far fa-clipboard','fas fa-bars'],
+    }
     // 1# JUMBOTRON OBJECT-database
     // 2# TEMI OBJECT-database
     // 3# TRUSTED OBJECT-database
