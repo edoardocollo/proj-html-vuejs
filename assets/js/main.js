dc4026ba393a1e2cad7f0eabd2b39ad9bc07feac
edoardocollo <edoardo.collo@gmail.com>
2021-01-19 14:05:31 +0100

    jumbotron structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,35 @@ let app = new Vue({
       logoImg: 'assets/img/logos/logo-light.png',
       links:['home','courses','insctructors','event','pages','elements'],
       icons:['fas fa-search','far fa-clipboard','fas fa-bars'],
-    }
+    },
     // 1# JUMBOTRON OBJECT-database
+    jumbotron:{
+      slide1:{
+        visible: true,
+        title: 'Contemporary Ideas',
+        text: 'blablablablabla',
+        button: true,
+        buttonText: 'register now',
+        background: 'path',
+      },
+      slide2:{
+        visible: false,
+        title: 'Contemporary Ideas',
+        text: 'blablablablabla',
+        button: true,
+        buttonText: 'register now',
+        background: 'path',
+      },
+      slide3:{
+        visible: false,
+        title: 'Contemporary Ideas',
+        text: 'blablablablabla',
+        button: true,
+        buttonText: 'register now',
+        background: 'path',
+      },
+
+    },
     // 2# TEMI OBJECT-database
     // 3# TRUSTED OBJECT-database
     // 4# EMPOWERING OBJECT-database
